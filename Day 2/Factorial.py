103180d3c560d89f3071a710f8b8c55c41a4e27d
nishanthkj <nishanthkj12@gmail.com>
2024-11-10 22:32:49 +0530

    Day 1

--- Day 2/Factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    assert n >= 0 and int(n) == n, 'The number must be a positive integer!'
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-    
-print(factorial(3))
--- Day 2/Fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-    assert n >= 0 and int(n) == n, 'Fibonacci number cannot be negative or non-integer.'
-    if n == 0 or n == 1:
-        return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-    
-print(fibonacci(3))
